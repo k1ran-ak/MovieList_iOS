db599d6f22b3b74f4c4a76d704633208a89937fa
Kiran <anushkiran7@gmail.com>
2022-07-18 17:50:55 +0530

    Added Movie List View and Movie Detail View

---
+++ movie_list_app/XIBs/MovieListTVC.swift
@@ -0,0 +1,16 @@
+//
+//  MovieListTVC.swift
+//  movie_list_app
+//
+//  Created by Kiran on 18/07/22.
+//
+
+import UIKit
+
+class MovieListTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
