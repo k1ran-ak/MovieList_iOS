e5750a9393f4f1b44fd00999194e05bb184832f4
Kiran <anushkiran7@gmail.com>
2022-07-18 16:40:19 +0530

    Initial Commit

---
+++ movie_list_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movie_list_app
+//
+//  Created by Kiran on 18/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
